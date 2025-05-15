a74ecff462394a53e9a4e1801798267f255ecdab
nahidhasan <nahid@nahidhasans-MacBook-Pro.local>
2025-05-16 00:59:20 +0600

    terraform module

--- web-app/main.tf
+++ web-app/main.tf
@@ -1,12 +1,12 @@
 
 terraform {
-    backend "s3" {
-        bucket = "integration-s3-bucket"
-        key = "integration-s3-bucket/terraform"
-        region = "us-east-2"
-        use_lockfile = true
-        encrypt = true
-    }
+    # backend "s3" {
+    #     bucket = "integration-s3-bucket"
+    #     key = "integration-s3-bucket/terraform"
+    #     region = "us-east-2"
+    #     use_lockfile = true
+    #     encrypt = true
+    # }
 
     required_providers {
     aws = {
@@ -20,58 +20,38 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "web_app" {
-  count = 2 # Number of instances to create
-  ami           = var.ami
-  instance_type = var.instance_type
-  security_groups = [aws_security_group.instance.name]
-  user_data = <<-EOF
-            #!/bin/bash
-            echo "Hello, World" > index.html
-            python3 -m http.server 8080 &
-            EOF
-
-  tags = {
-    Name = "Server ${count.index + 1}"
-  }
-}
-
-resource "aws_s3_bucket" "bucket" {
-  bucket = "integration-s3-bucket"
-  acl    = "private"
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "ExampleS3Bucket"
-    Environment = "Dev"
-  }
-}
 
 data "aws_vpc" "default_vpc" {
   default = true
 }
 
-resource "aws_security_group_rule" "allow_http_inbound" {
-  description = "Allow HTTP inbound traffic"
-  type        = "ingress"
-  security_group_id = aws_security_group.instance.id
-  from_port   = 8080
-  to_port     = 8080
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
+resource "aws_security_group" "instance" {
+  name        = "web-app-sg"
+  description = "Security group for web app"
 
-resource "aws_db_instance" "db_instance" {
-    identifier = "example-db-instance"
-    engine     = "mysql"
-    instance_class = "db.t2.micro"
-    allocated_storage = 20
-    username   = var.db_username
-    password   = var.db_password
-    db_name    = "exampledb"
-    skip_final_snapshot = true
-    publicly_accessible = true
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WebAppSecurityGroup"
+  }
+}
+module "web_app" {
+    source = "../modules/web-app"
+    ami = var.ami
+    instance_type = var.instance_type
+    db_username = var.db_username
+    db_password = var.db_password
+    security_group_id = aws_security_group.instance.id
 }
--- web-app/output.tf
+++
@@ -1,4 +0,0 @@
-output "db_instance_address" {
-  value = aws_db_instance.db_instance.address
-  description = "The address of the database instance"
-}
